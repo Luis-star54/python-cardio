9efd0a1c270a24e8f19ae78ef9ff8f89061b93c9
luis <luisortegaa4222@gmail.com>
2021-05-13 10:38:58 -0500

    programa rangos cambiantes finalizado

--- piedra_papel_tijera/main.py
+++ piedra_papel_tijera/main.py
@@ -3,13 +3,8 @@ os.system('clear')
 
 
     
-
 def run():
     
 
-
-    
-
 if __name__ == '__main__':
-    print('Bienvenidx al juego de piedra papel o tijera')
     run()
--- rangos_cambiantes/main.py
+++ rangos_cambiantes/main.py
@@ -8,16 +8,16 @@ def run():
     limite_superior = int(input('Elige un limite superior => '))
     comparacion = int(input('Elige un numero  => '))
 
-    for i in range(limite_inferior, limite_superior + 1):
-        if i > limite_inferior and i < limite_superior:
-            print(comparacion)
+    for numero in range(limite_inferior , limite_superior + 1):
+        
+        if comparacion > limite_inferior and comparacion < limite_superior:
+            if comparacion == numero:
+                print(comparacion)
         else:
-            comparacion = int(input('Elige otro numero => '))
-
-
+            comparacion = int(input('elige otro numero => '))
+            if comparacion == numero:
+                print(comparacion)
                 
-
-
-
+        
 if __name__ == '__main__':
     run()
